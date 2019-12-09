7c61a3e5ca3a1ccee7aae7d067780bea6ae4ad1f
Sam <samp805@gmail.com>
2019-12-09 14:07:18 -0500

    removed middleware

--- post/forms.py
+++ post/forms.py
@@ -1,15 +1,8 @@
 from django.forms import ModelForm
 
-from django_currentuser.middleware import get_current_user
-
 from post.models import Post
 
 class PostForm(ModelForm):
-
-    def save(self, commit=True):
-        self.instance.author = get_current_user()
-        return super().save(commit)
-
 
     class Meta:
         model = Post
--- post/views.py
+++ post/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views.generic import CreateView, TemplateView
 
-from django_currentuser.middleware import get_current_user
-
 from post.models import Post
 from post.forms import PostForm
 
@@ -13,6 +11,11 @@ class CreatePostView(CreateView):
     form_class = PostForm
     template_name = 'posting/post_form.html'
     success_url = 'confirm'
+
+    def form_valid(self, form):
+        # auto populate author field
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class ConfirmPostView(TemplateView):
