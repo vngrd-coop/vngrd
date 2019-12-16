1c2765b1246708f9f9ff7fe1907894b439221ae3
Sam <samp805@gmail.com>
2019-12-16 10:47:01 -0500

    logout done

--- vngrd-frontend/src/Component/Navigator.js
+++ vngrd-frontend/src/Component/Navigator.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from './Hooks/useAuth';
 
-const Navigator = () => (
-    <nav>
-        <ul>
-            <li><NavLink to='/'>Home</NavLink></li>
-            <li><NavLink to='/login'>Log In</NavLink></li>
-            <li><NavLink to='/signup'>Sign Up</NavLink></li>
-        </ul>
-    </nav>
-);
+const Navigator = () => {
+    const auth = useAuth();
+
+    return (
+        <nav>
+            { auth.user ? (
+                <ul>
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li onClick={() => auth.logout()}>Log Out</li>
+                </ul>
+                ) : (
+                <ul>
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink to='/login'>Log In</NavLink></li>
+                    <li><NavLink to='/signup'>Sign Up</NavLink></li>
+                </ul>
+            )}
+        </nav>
+    );
+}
 
 export default Navigator;
